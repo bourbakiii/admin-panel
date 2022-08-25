0591a0df36ea4c557271d5ce96d72d689b7c0d1f
bourbakiii <gabisov03912@gmail.com>
2022-08-25 17:26:46 +0300

    css and base layout

---
+++ src/components/Navigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navigation">
+
+  </nav>
+</template>
+<style lang="scss">
+.navigation{
+  width:100%;
+  height:100px;
+  background-color: rgba(125,125,125);
+  border-radius: 0 15px 15px 0;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+              @import "./src/assets/scss/global.scss";
+            `
+            }
+        }
+    }
 })
+
