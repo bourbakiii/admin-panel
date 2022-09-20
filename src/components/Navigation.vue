79b5e7ea61722e09928cf5a8d81befe90471b722
bourbakiii <gabisov03912@gmail.com>
2022-09-20 19:10:01 +0300

    Initial Pinia

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,8 +1,18 @@
 <template>
-  <header class="navigation">
+  <header @click="$INDEX_STORE.increment()" class="navigation">
     Some text right there
+    {{$INDEX_STORE}}
   </header>
 </template>
+<script setup>
+import {useIndexStore} from '@/stores/index'
+const $INDEX_STORE = useIndexStore()
+$INDEX_STORE.count++
+// with autocompletion ✨
+$INDEX_STORE.$patch({count: $INDEX_STORE.count + 1})
+// or using an action instead
+$INDEX_STORE.increment()
+</script>
 <style lang="scss">
 // !!! padding-left должен быть больше padding-right на расстояние указанное в translateX(-XX)
 .navigation {
@@ -16,4 +26,5 @@
   align-items: center;
   flex-direction: row;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import {createApp} from 'vue'
 import 'reset-css'
 import App from './App.vue'
 import router from './router/router.js'
+import {createPinia} from 'pinia'
+
 
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {fas} from '@fortawesome/free-solid-svg-icons'
 
+
 library.add(fas)
 
 
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(createPinia()).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
